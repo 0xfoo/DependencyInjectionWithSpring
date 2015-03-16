774b83210df5607864b7ed6b9bed96504713024a
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-16 20:56:35 +0100

    Used different versions of injecting dependencies. Also added some explonation.

--- src/main/java/pc/config/PCConfig.java
+++ src/main/java/pc/config/PCConfig.java
@@ -1,9 +1,11 @@
 package pc.config;
 
-import internet.InternetConnection;
-import internet.InternetConnectionMock;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import static org.mockito.Mockito.*;
+
+import internet.InternetConnection;
+import internet.InternetConnectionMock;
 import pc.PC;
 import periphery.*;
 
@@ -16,18 +18,28 @@ public class PCConfig {
     }
 
     @Bean
+    // injecting the 'actual' InternetConnectionMock (derived from the InternetConnection interface)
     public InternetConnection internetConnection() {
         return new InternetConnectionMock();
     }
 
     @Bean
+    // injecting a Monitor that is working (default is not) even though Monitor is not an interface
     public Monitor monitor() {
-        return new Monitor();
+        return new Monitor() {
+            public boolean isWorking(){
+                return true;
+            }
+        };
     }
 
     @Bean
+    // injecting a mock of PowerSupply through mockito mock
     public PowerSupply powerSupply() {
-        return new PowerSupply();
+        PowerSupply mockPowerSupply = mock(PowerSupply.class);
+        // default behaviour is false for boolean functions
+        when(mockPowerSupply.isOn()).thenReturn(true);
+        return mockPowerSupply;
     }
 
     @Bean
