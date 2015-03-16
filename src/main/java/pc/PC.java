b6e20f805e3eb404bbba9a789753da48105072e2
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-16 20:37:42 +0100

    Made PC constructor public (for configuration) and minor style changes.

--- src/main/java/pc/PC.java
+++ src/main/java/pc/PC.java
@@ -20,46 +20,46 @@ public class PC {
      * Constructor for PC need power supply, monitor, internet connection, keyboard and mouse
      *
      * @param internetConnection internet connection
-     * @param monitor monitor
-     * @param keyboardAndMouse keyboard and mouse
-     * @param powerSupply power supply
+     * @param monitor            monitor
+     * @param keyboardAndMouse   keyboard and mouse
+     * @param powerSupply        power supply
      */
     @Autowired
-    PC(InternetConnection internetConnection, Monitor monitor,
-       KeyboardAndMouse keyboardAndMouse, PowerSupply powerSupply){
+    public PC(InternetConnection internetConnection, Monitor monitor,
+              KeyboardAndMouse keyboardAndMouse, PowerSupply powerSupply) {
         this.internetConnection = internetConnection;
         this.monitor = monitor;
         this.keyboardAndMouse = keyboardAndMouse;
         this.powerSupply = powerSupply;
     }
 
-    boolean isConnectedToTheInternet(){
+    boolean isConnectedToTheInternet() {
         return internetConnection.isOnline();
     }
 
-    boolean isMonitorWorking(){
+    boolean isMonitorWorking() {
         return monitor.isWorking();
     }
 
-    boolean isKeyboardAndMousePluggedIn(){
+    boolean isKeyboardAndMousePluggedIn() {
         return keyboardAndMouse.isPlugged();
     }
 
-    boolean isPowerSupplied(){
+    boolean isPowerSupplied() {
         return powerSupply.isOn();
     }
 
-    public void printStatusReport(){
-        String internetStatus = isConnectedToTheInternet() ? "": "not" + " connected.";
+    public void printStatusReport() {
+        String internetStatus = isConnectedToTheInternet() ? "" : "not" + " connected.";
         System.out.println("Internet is " + internetStatus);
 
-        String monitorStatus = isMonitorWorking() ? "": "not" + " working.";
+        String monitorStatus = isMonitorWorking() ? "" : "not" + " working.";
         System.out.println("Monitor is " + monitorStatus);
 
-        String keyboardAndMouseStatus = isKeyboardAndMousePluggedIn() ? "": "not" + " plugged.";
+        String keyboardAndMouseStatus = isKeyboardAndMousePluggedIn() ? "" : "not" + " plugged.";
         System.out.println("Keyboard and Mouse are " + keyboardAndMouseStatus);
 
-        String powerSupplyStatus = isPowerSupplied() ? "": "not" + " supplied.";
+        String powerSupplyStatus = isPowerSupplied() ? "" : "not" + " supplied.";
         System.out.println("Power is " + powerSupplyStatus);
     }
 }
