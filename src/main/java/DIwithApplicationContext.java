0c00d0cf52d45bdba0658bf0d73efd98cba54f19
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-16 20:39:19 +0100

    Updated DIwithApplicationContext to use configuration from newly added PCConfig.

--- src/main/java/DIwithApplicationContext.java
+++ src/main/java/DIwithApplicationContext.java
@@ -1,28 +1,14 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-import internet.InternetConnection;
 import pc.PC;
+import pc.config.PCConfig;
 
-@Configuration
-@ComponentScan
 public class DIwithApplicationContext {
-
-    @Bean
-    InternetConnection mockInternetConnection() {
-        return new InternetConnection(){
-            public boolean isOnline(){
-                return false;
-            }
-        };
-    }
 
     public static void main(String[] args) {
         ApplicationContext context =
-                new AnnotationConfigApplicationContext(DIwithApplicationContext.class);
+                new AnnotationConfigApplicationContext(PCConfig.class);
         PC pc = context.getBean(PC.class);
         pc.printStatusReport();
     }
