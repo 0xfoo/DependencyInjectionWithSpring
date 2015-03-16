bfd3ca7fbdcd95c5160df1df8b468abf4e202dd2
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-16 20:54:48 +0100

    Added comment to Monitor.isWorking().

--- src/main/java/periphery/Monitor.java
+++ src/main/java/periphery/Monitor.java
@@ -1,6 +1,12 @@
 package periphery;
 
 public class Monitor {
+
+    /**
+     * Unlucky the Monitor never works
+     *
+     * @return false
+     */
     public boolean isWorking() {
         return false;
     }
