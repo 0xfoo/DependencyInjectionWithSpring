5e86e5946bbce0b7b98e82d270af4ce18ac9ca10
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-19 22:05:47 +0100

    Added new test case to reflect new config usage.

--- src/test/java/pc/PCTest.java
+++ src/test/java/pc/PCTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import pc.config.AllInOneConfig;
 import pc.config.PCConfig;
 
 import static org.junit.Assert.*;
@@ -11,7 +12,18 @@ import static org.junit.Assert.*;
 public class PCTest {
 
     @Test
-    public void testPCCreationThroughApplicationContext() {
+    public void testPCCreationThroughAllInOneJavaConfig() {
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext(AllInOneConfig.class);
+        PC pc = context.getBean(PC.class);
+        assertTrue(pc.isConnectedToTheInternet());
+        assertTrue(pc.isMonitorWorking());
+        assertTrue(pc.isKeyboardAndMousePluggedIn());
+        assertTrue(pc.isPowerSupplied());
+    }
+
+    @Test
+    public void testPCCreationThroughModularJavaConfig() {
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(PCConfig.class);
         PC pc = context.getBean(PC.class);
@@ -22,7 +34,7 @@ public class PCTest {
     }
 
     @Test
-    public void testPCCreationThroughXMLContext() {
+    public void testPCCreationThroughXMLConfig() {
         ClassPathXmlApplicationContext context =
                 new ClassPathXmlApplicationContext("pc.xml");
         PC pc = context.getBean(PC.class);
