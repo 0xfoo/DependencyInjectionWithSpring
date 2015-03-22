adcf8fa10145e2db4d8bea060885dc38a8c9a333
Christian Rotzoll <ch.rotzoll@gmail.com>
2015-03-22 21:00:52 +0100

    Added test case using auto wired test object (PC).

--- src/test/java/pc/PCTest.java
+++ src/test/java/pc/PCTest.java
@@ -1,15 +1,32 @@
 package pc;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import pc.config.AllInOneConfig;
 import pc.config.PCConfig;
 
 import static org.junit.Assert.*;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=AllInOneConfig.class)
 public class PCTest {
+
+    @Autowired
+    PC testPC;
+
+    @Test
+    public void testPCCreationThroughAutoWiring() {
+        assertTrue(testPC.isConnectedToTheInternet());
+        assertTrue(testPC.isMonitorWorking());
+        assertTrue(testPC.isKeyboardAndMousePluggedIn());
+        assertTrue(testPC.isPowerSupplied());
+    }
 
     @Test
     public void testPCCreationThroughAllInOneJavaConfig() {
